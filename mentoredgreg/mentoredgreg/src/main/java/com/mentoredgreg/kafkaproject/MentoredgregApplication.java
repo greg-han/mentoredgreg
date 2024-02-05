b23ae7dc176ef01e339a1d38a8cd44d5936a6a0a
Greg <24735588+greg-han@users.noreply.github.com>
2024-02-04 16:09:08 -0800

    trying to get started

---
+++ mentoredgreg/mentoredgreg/src/main/java/com/mentoredgreg/kafkaproject/MentoredgregApplication.java
@@ -0,0 +1,13 @@
+package com.mentoredgreg.kafkaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentoredgregApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentoredgregApplication.class, args);
+	}
+
+}
---
+++ mentoredgreg/mentoredgreg/src/test/java/com/mentoredgreg/kafkaproject/MentoredgregApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentoredgreg.kafkaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentoredgregApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

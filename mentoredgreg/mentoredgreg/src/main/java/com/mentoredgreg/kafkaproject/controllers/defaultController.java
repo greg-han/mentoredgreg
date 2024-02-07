abf7975e2693c60708b71e94189fa494107b5533
Gregory Han <gregbook@Gregorys-MacBook-Air.local>
2024-02-07 12:21:54 -0800

    attempting to add kafka functionality

---
+++ mentoredgreg/mentoredgreg/src/main/java/com/mentoredgreg/kafkaproject/controllers/defaultController.java
@@ -0,0 +1,20 @@
+package com.mentoredgreg.kafkaproject.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class defaultController {
+
+    @GetMapping("/error")
+    public ResponseEntity<String> publish (){
+       System.out.println("Error!");
+
+        ResponseEntity<String> error = ResponseEntity.ok("Error");
+        return error;
+    }
+}
